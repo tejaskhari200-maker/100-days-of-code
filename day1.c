fb9610dd61c3c82e3285db7c31ab78f11fbdff3c
tejaskhari200-maker <tejaskhari200@gmail.com>
2025-09-11 21:43:54 +0530

    Add files via upload

---
+++ day1.c
@@ -0,0 +1,14 @@
+ #include <stdio.h>
+int main()
+{
+    int a,b , sum;
+    printf("enter first number:");
+    scanf("%d", &a);
+    printf("enter  2nd number:");
+    scanf("%d", &b);
+
+    sum=a+b;
+    printf("sum of the numbers is: %d", sum);
+
+    return 0;
+}
