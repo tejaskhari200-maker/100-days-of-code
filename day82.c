15009de4437c601fdf7e5946443573b9249896f0
Tejas khari <tejaskhari200@gmail.com>
2025-11-11 20:49:39 +0530

    Add files via upload

---
+++ day82.c
@@ -0,0 +1,16 @@
+//print the total numbers of pairs in the array whose sum is equal to the given value x
+#include<stdio.h>
+int main(){
+    int arr[5]={2,3,4,5,6};/*initatialisation of an array */
+    int x=10;
+    int sum;
+    for(int i=0;i<=5;i++){
+sum=sum+arr[i];
+
+    }
+
+
+
+    return 0;
+
+}
