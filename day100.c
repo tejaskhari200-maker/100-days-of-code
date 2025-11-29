ea4eb2880fc9a6b6a7ab859910c35112b44551a7
Tejas khari <tejaskhari200@gmail.com>
2025-11-29 22:03:47 +0530

    Add files via upload

---
+++ day100.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int roll;
+    int marks;
+};
+
+int main() {
+    struct Student s;
+    struct Student *ptr = &s;
+
+    // Input format: Name Roll Marks
+    scanf("%s %d %d", ptr->name, &ptr->roll, &ptr->marks);
+
+    printf("Modified Data: Name: %s | Roll: %d | Marks: %d",
+           ptr->name, ptr->roll, ptr->marks);
+
+    return 0;
+}
